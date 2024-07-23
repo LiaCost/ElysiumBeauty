6df752d733c6f796f333a3582a3b665ea767895a
jvduartte <lia.c.santtos@gmail.com>
2024-07-23 17:43:02 -0300

    UP02

--- obj/Release/net8.0/ElysiumBeauty.AssemblyInfo.cs
+++ obj/Release/net8.0/ElysiumBeauty.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ElysiumBeauty")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4e91746275cce32d87d93f0e9da24e3271717d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ElysiumBeauty")]
